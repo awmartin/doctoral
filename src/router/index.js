ed830fdb0ab7d44292695f4f0933afc394c88d9c
William Martin <awmartin@gmail.com>
2020-04-07 19:47:30 -0400

    Direct navigation to docs by url

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,6 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/Home.vue'
 
-const fb = require('../firebase.js')
-const _ = require('lodash')
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -54,14 +51,14 @@ const routes = [
     }
   },
 
-  {
-    path: '/folder/:id',
-    name: 'Folder',
-    component: () => import(/* webpackChunkName: "document" */ '../views/Folder.vue'),
-    meta: {
-      requiresAuth: true
-    }
-  },
+  // {
+  //   path: '/folder/:id',
+  //   name: 'Folder',
+  //   component: () => import(/* webpackChunkName: "document" */ '../views/Folder.vue'),
+  //   meta: {
+  //     requiresAuth: true
+  //   }
+  // },
 ]
 
 const router = new VueRouter({
@@ -70,22 +67,25 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-  const currentUser = fb.getCurrentUser()
-  const isLoggedIn = !_.isNil(currentUser) // This is much faster than store.getters.isLoggedIn. Seems to be sufficient.
+// router.beforeEach((to, from, next) => {
+//   const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+//   const currentUser = fb.getCurrentUser()
+//   const isLoggedIn = !_.isNil(currentUser) // This is much faster than store.getters.isLoggedIn. Seems to be sufficient.
 
-  if (to.name === from.name && _.isEqual(to.params, from.params)) { return }
+//   console.debug('beforeEach', isLoggedIn)
 
-  if (requiresAuth && !isLoggedIn) {
-    next('/')
-  } else if (requiresAuth && isLoggedIn) {
-    // Regular application pages which require authentication.
-    next()
-  } else {
-    // Public page. Doesn't require the user to be logged in.
-    next()
-  }
-})
+//   // Prevents an issue when a route is tripped twice. No need to navigate again.
+//   if (to.name === from.name && _.isEqual(to.params, from.params)) { return }
+
+//   if (requiresAuth && !isLoggedIn) {
+//     next('/')
+//   } else if (requiresAuth && isLoggedIn) {
+//     // Regular application pages which require authentication.
+//     next()
+//   } else {
+//     // Public page. Doesn't require the user to be logged in.
+//     next()
+//   }
+// })
 
 export default router
