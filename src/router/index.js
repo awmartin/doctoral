8c1b9762979be4289825233a75fe792f1f02ea64
William Martin <awmartin@gmail.com>
2020-04-11 17:33:24 -0400

    Some code cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '@/components/Home.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -58,16 +58,7 @@ const routes = [
     meta: {
       requiresAuth: true
     }
-  },
-
-  // {
-  //   path: '/folder/:id',
-  //   name: 'Folder',
-  //   component: () => import(/* webpackChunkName: "document" */ '../views/Folder.vue'),
-  //   meta: {
-  //     requiresAuth: true
-  //   }
-  // },
+  }
 ]
 
 const router = new VueRouter({
@@ -75,26 +66,5 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
-// router.beforeEach((to, from, next) => {
-//   const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-//   const currentUser = fb.getCurrentUser()
-//   const isLoggedIn = !_.isNil(currentUser) // This is much faster than store.getters.isLoggedIn. Seems to be sufficient.
-
-//   console.debug('beforeEach', isLoggedIn)
-
-//   // Prevents an issue when a route is tripped twice. No need to navigate again.
-//   if (to.name === from.name && _.isEqual(to.params, from.params)) { return }
-
-//   if (requiresAuth && !isLoggedIn) {
-//     next('/')
-//   } else if (requiresAuth && isLoggedIn) {
-//     // Regular application pages which require authentication.
-//     next()
-//   } else {
-//     // Public page. Doesn't require the user to be logged in.
-//     next()
-//   }
-// })
 
 export default router
