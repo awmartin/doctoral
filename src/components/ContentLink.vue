a0ab7107feb23d2aa3a3bd20079b693f1b2a48f5
William Martin <awmartin@gmail.com>
2020-04-12 11:09:00 -0400

    Refactored ContentLink component

--- src/components/ContentLink.vue
+++ src/components/ContentLink.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="content-link">
-    <a @click="handleClick" :class="contentClass">
+  <div :class="contentClass">
+    <a @click="handleClick">
       <slot name="icon">
         <file-document-outline-icon v-if="isDocument" />
         <folder-outline-icon v-if="isFolder" />
@@ -17,62 +17,68 @@
 a {
   display: block;
   cursor: pointer;
+}
+.Folder {
+  font-weight: 500;
+}
 
-  &.block {
+.block {
+  a {
     padding: 6px 8px 6px 10px;
-
-    &.normal:hover {
-      background-color: lighten(lightskyblue, 10%);
-    }
-    &.normal:active {
-      background-color: lightskyblue;
-    }
-
-    &.disabled {
-      color: lightgray;
-    }
-    &.disabled:hover {
-      background-color: none !important;
-    }
-    &.disabled:active {
-      background-color: none !important;
-    }
-
-    &.selected {
-      background-color: lighten(lightskyblue, 20%);
-    }
-    &.selected:hover {
-      background-color: lighten(lightskyblue, 10%);
-    }
+  }
+  &.normal a:hover {
+    background-color: lighten(lightskyblue, 10%);
+  }
+  &.normal a:active {
+    background-color: lightskyblue;
   }
 
-  &.underline {
+  &.disabled a {
+    color: lightgray;
+  }
+  &.disabled a:hover {
+    background-color: none !important;
+  }
+  &.disabled a:active {
+    background-color: none !important;
+  }
+
+  &.selected a {
+    background-color: lighten(lightskyblue, 20%);
+  }
+  &.selected a:hover {
+    background-color: lighten(lightskyblue, 10%);
+  }
+}
+
+.underline {
+  a {
     padding: 1px 2px 1px 3px;
     margin: 5px 6px 5px 7px;
+  }
 
-    &.normal:hover {
-      border-bottom: 1px solid lighten(lightskyblue, 10%);
-    }
-    &.normal:active {
-      border-bottom: 1px solid lightskyblue;
-    }
+  &.normal a:hover {
+    border-bottom: 1px solid lighten(lightskyblue, 10%);
+  }
+  &.normal a:active {
+    border-bottom: 1px solid lightskyblue;
+  }
 
-    &.disabled {
-      color: lightgray;
-    }
-    &.disabled:hover {
-      border-bottom: 1px solid none !important;
-    }
-    &.disabled:active {
-      border-bottom: 1px solid none !important;
-    }
+  &.disabled a {
+    color: lightgray;
+  }
+  &.disabled a:hover {
+    border-bottom: 1px solid transparent !important;
+  }
+  &.disabled a:active {
+    border-bottom: 1px solid transparent !important;
+  }
 
-    &.selected {
-      border-bottom: 1px solid lighten(lightskyblue, 20%);
-    }
-    &.selected:hover {
-      border-bottom: 1px solid lighten(lightskyblue, 10%);
-    }
+  &.selected a {
+    border-bottom: 1px solid lighten(lightskyblue, 20%);
+  }
+  &.selected a:hover {
+    border-bottom: 1px solid lighten(lightskyblue, 10%);
   }
 }
 </style>
@@ -142,11 +148,11 @@ export default {
 
     contentClass () {
       if (_.isFunction(this.disabled) && this.disabled(this.content)) {
-        return `${this.contentType} ${this.options.highlightStyle} disabled`
+        return `content-link ${this.contentType} ${this.options.highlightStyle} disabled`
       } else if (this.routeId === this.content.key || this.routeId === this.content.id) {
-        return `${this.contentType} ${this.options.highlightStyle} selected`
+        return `content-link ${this.contentType} ${this.options.highlightStyle} selected`
       } else {
-        return `${this.contentType} ${this.options.highlightStyle} normal`
+        return `content-link ${this.contentType} ${this.options.highlightStyle} normal`
       }
     },
 
