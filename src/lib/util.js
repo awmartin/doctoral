2dfdb25c6b70704cce9c0fcb01163573d60d0dab
William Martin <awmartin@gmail.com>
2020-04-22 17:26:32 -0400

    Fix to URL sanitizer

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 function getTitleUrl (content) {
   const trimTitle = _.trim(content.title)
   const titleForUrl = _.replace(trimTitle, /[^a-zA-Z0-9]/g, '-')
-  const sanitizedTitleUrl = _.replace(titleForUrl, /[0]{1,}/, '-')
+  const sanitizedTitleUrl = _.replace(titleForUrl, /[-]{1,}/g, '-')
   const titleUrl = _.trim(sanitizedTitleUrl, '-')
   return titleUrl
 }
