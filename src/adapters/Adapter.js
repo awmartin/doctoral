4de7e6844e678798adf0855dec4efe59975c1800
William Martin <awmartin@anomalus.com>
2021-12-06 16:25:27 -0500

    Consume args in Adapter method stubs

--- src/adapters/Adapter.js
+++ src/adapters/Adapter.js
@@ -8,6 +8,7 @@ class Adapter {
   }
 
   registerAuthCallback (onLoggedIn, onNotLoggedIn) {
+    _.noop(onLoggedIn, onNotLoggedIn)
     throw new Error('Implement Adapter.registerAuthCallback')
   }
 
@@ -28,22 +29,27 @@ class Adapter {
   }
 
   updateUser (data) {
+    _.noop(data)
     throw new Error('Implement Adapter.updateUser')
   }
 
   registerContentsListener (onUpdate) {
+    _.noop(onUpdate)
     throw new Error('Implement Adapter.registerContentsListener')
   }
 
   registerTrashListener (onUpdate) {
+    _.noop(onUpdate)
     throw new Error('Implement Adapter.registerTrashListener')
   }
 
   registerArchiveListener (onUpdate) {
+    _.noop(onUpdate)
     throw new Error('Implement Adapter.registerArchiveListener')
   }
 
   registerUserStateListener (onUpdate) {
+    _.noop(onUpdate)
     throw new Error('Implement Adapter.registerUserStateListener')
   }
 
@@ -58,42 +64,52 @@ class Adapter {
   }
 
   createContent (content, parent, batch_ = null) {
+    _.noop(content, parent, batch_)
     throw new Error('Implement Adapter.createContent')
   }
 
   updateContent (content, batch_ = null) {
+    _.noop(content, batch_)
     throw new Error('Implement Adapter.updateContent')
   }
 
   createDocument (document, parent) {
+    _.noop(document, parent)
     throw new Error('Implement Adapter.createDocument')
   }
 
   updateDocument (document) {
+    _.noop(document)
     throw new Error('Implement Adapter.updateDocument')
   }
 
   loadDocument (documentKey) {
+    _.noop(documentKey)
     throw new Error('Implement Adapter.loadDocument')
   }
 
   publishDocument (document, slug) {
     // Optional.
+    _.noop(document, slug)
   }
 
   delete (items) {
+    _.noop(items)
     throw new Error('Implement Adapter.delete')
   }
 
   loadTagSnippets (hashtag) {
+    _.noop(hashtag)
     throw new Error('Implement Adapter.loadTagSnippets')
   }
 
   updateTagSnippets (content, batch_ = null) {
+    _.noop(content, batch_)
     throw new Error('Implement Adapter.updateTagSnippets')
   }
 
   uploadFileForDocument (file, document) {
+    _.noop(file, document)
     throw new Error('Implement Adapter.uploadFileForDocument')
   }
 }
