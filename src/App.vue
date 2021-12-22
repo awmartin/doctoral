bb2298c00c1d18d38b69e2027576ea6b8999cf28
William Martin <awmartin@anomalus.com>
2021-12-22 00:42:17 -0500

    Fix to sidebar footer position

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,13 @@
 </template>
 
 <style lang="scss">
-main#app {
+main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   height: 100%;
+  width: 100%;
 }
 </style>
 
